a0e2e20ec1b500a865cb864ca50658eb3d570911
Mikhail <47395053+MikhailDeriabin@users.noreply.github.com>
2023-01-06 19:40:49 +0200

    Delete teht9 directory

--- teht9/src/main/java/org/example/IHinnoittelija.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public interface IHinnoittelija {
-	float getAlennusProsentti(Asiakas asiakas, Tuote tuote);
-	void aloita();
-	void setAlennusProsentti(Asiakas asiakas, float prosentti);
-	void lopeta();
-}

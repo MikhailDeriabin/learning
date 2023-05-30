07fe4c96e80180bfd3a91d5f34f51f4b558312d8
Mikhail <mikko.deriabin@gmail.com>
2023-05-30 18:27:19 +0300

    add 3.1 task react_viope

---
+++ react_viope/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+        <div className='App'>
+            <h1>Welcome to my site</h1>
+            <p>This is my first React app</p>
+        </div>
+    </React.Fragment>
+  );
+}
+
+export default App;

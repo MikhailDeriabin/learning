d0b204689b90e4270dd38568737631d298b838e5
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-11-08 15:42:33 +0200

    add 9th problem

---
+++ leetcode/cs/leetcode/s9/Runner.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcode.s9;
+
+using System;
+
+public class Runner
+{
+    public bool IsPalindrone(int x)
+    {
+        if(x < 0)
+            return false;
+        
+        var strX = x.ToString();
+
+        return strX == Reverse(strX);
+    }
+    
+    private string Reverse( string s )
+    {
+        char[] charArray = s.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
---
+++ leetcode/cs/leetcode/s9/RunnerTest.cs
@@ -0,0 +1,43 @@
+﻿namespace leetcode.s9;
+
+using Xunit;
+
+public class RunnerTest
+{
+    private readonly Runner _runner;
+
+    public RunnerTest()
+    {
+        _runner = new Runner();
+    }
+    
+    [Fact]
+    public void Test1()
+    {
+        var input = 121;
+
+        var actual = _runner.IsPalindrone(input);
+
+        Assert.True(actual);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var input = -121;
+
+        var actual = _runner.IsPalindrone(input);
+
+        Assert.False(actual);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var input = 10;
+
+        var actual = _runner.IsPalindrone(input);
+
+        Assert.False(actual);
+    }
+}

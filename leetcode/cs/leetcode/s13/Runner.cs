f38b6e01285418cb9d6cba6aef65eb9643fcae26
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-11-08 16:26:58 +0200

    add problem 13

---
+++ leetcode/cs/leetcode/s13/Runner.cs
@@ -0,0 +1,71 @@
+﻿namespace leetcode.s13;
+
+using System.Collections.Generic;
+using System.Linq;
+
+public class Runner
+{
+    private Dictionary<char, int> _romanNumbers = new()
+    {
+        { 'I', 1 },
+        { 'V', 5 },
+        { 'X', 10 },
+        { 'L', 50 },
+        { 'C', 100 },
+        { 'D', 500 },
+        { 'M', 1000 }
+    };
+
+    public int RomanToInt(string romanNumber)
+    {
+        if (!IsRomanNumber(romanNumber))
+        {
+            return 0;
+        }
+
+        var result = 0;
+        
+        var letters = romanNumber.ToCharArray();
+        var length = letters.Length;
+
+        for(var i = 0; i < length; i++)
+        {
+            if (i + 1 >= length)
+            {
+                result += _romanNumbers[letters[i]];
+                break;
+            }
+                
+            var current = _romanNumbers[letters[i]];
+            var next = _romanNumbers[letters[i+1]];
+
+            if (current < next)
+            {
+                result += next - current;
+                i++;
+                continue;
+            }
+            
+            result += current;
+        }
+        
+        return result;
+    }
+
+    private bool IsRomanNumber(string romanNumber)
+    {
+        foreach (var letter in romanNumber)
+        {
+            if (!IsRomanLetter(letter))
+                return false;
+        }
+        return true;
+    }
+
+    private bool IsRomanLetter(char letter)
+    {
+        var allowedLetters = new List<char>{'I', 'V', 'X', 'L', 'C', 'D', 'M'};
+        
+        return allowedLetters.Contains(letter);
+    }
+}
---
+++ leetcode/cs/leetcode/s13/RunnerTest.cs
@@ -0,0 +1,46 @@
+﻿namespace leetcode.s13;
+
+using Xunit;
+
+public class RunnerTest
+{
+    private readonly Runner _runner;
+
+    public RunnerTest()
+    {
+        _runner = new Runner();
+    }
+    
+    [Fact]
+    public void Test1()
+    {
+        var input = "III";
+        var expected = 3;
+
+        var actual = _runner.RomanToInt(input);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var input = "LVIII";
+        var expected = 58;
+
+        var actual = _runner.RomanToInt(input);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var input = "MCMXCIV";
+        var expected = 1994;
+
+        var actual = _runner.RomanToInt(input);
+
+        Assert.Equal(expected, actual);
+    }
+}

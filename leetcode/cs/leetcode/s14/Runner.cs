f22511b3e0da7fe6bb6c53185875f9a91f650266
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-11-09 15:20:57 +0200

    add solution 14

---
+++ leetcode/cs/leetcode/s14/Runner.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode.s14;
+
+using System.Collections.Generic;
+using System.Linq;
+
+public class Runner
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        if(strs.Length == 1)
+            return strs[0];
+        
+        var prefix = "";
+        var firstStrChars = strs[0].ToCharArray();
+        for (int i = 0; i < firstStrChars.Length; i++)
+        {
+            var character = firstStrChars[i];
+            for (int j = 1; j < strs.Length; j++)
+            {
+                var str = strs[j];
+                if(i >= str.Length || str[i] != character)
+                    return prefix;
+            }
+            prefix += character;
+        }
+        
+        return prefix;
+    }
+}
---
+++ leetcode/cs/leetcode/s14/RunnerTest.cs
@@ -0,0 +1,35 @@
+﻿namespace leetcode.s14;
+
+using Xunit;
+
+public class RunnerTest
+{
+    private readonly Runner _runner;
+
+    public RunnerTest()
+    {
+        _runner = new Runner();
+    }
+    
+    [Fact]
+    public void Test1()
+    {
+        string[] input = ["flower","flow","flight"];
+        var expected = "fl";
+
+        var actual = _runner.LongestCommonPrefix(input);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        string[] input = ["dog","racecar","car"];
+        var expected = "";
+
+        var actual = _runner.LongestCommonPrefix(input);
+
+        Assert.Equal(expected, actual);
+    }
+}

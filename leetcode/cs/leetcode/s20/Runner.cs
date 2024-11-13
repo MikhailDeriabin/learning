3b744651de81d2414726659d1b287718500ccfc3
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-11-13 17:50:46 +0200

    add part of s 20

---
+++ leetcode/cs/leetcode/s20/Runner.cs
@@ -0,0 +1,58 @@
+﻿namespace leetcode.s20;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Runner
+{
+    Dictionary<char, string> _openBrackets = new()
+    {
+        { '(', "0" },
+        { '[', "1" },
+        { '{', "2" }
+    };
+
+    Dictionary<char, string> _closedBrackets = new()
+    {
+        { ')', "0" },
+        { ']', "1" },
+        { '}', "2" }
+    };
+
+    public bool IsValid(string s)
+    {
+        var chars = s.ToCharArray();
+
+        var charsCount = chars.Length;
+
+        if (charsCount == 0 || charsCount % 2 != 0)
+            return false;
+
+        var foundOpenBrackets = "";
+        var foundClosedBrackets = "";
+
+        for (var i = 0; i < charsCount; i++)
+        {
+            var currentChar = chars[i];
+            
+            if(isOpenBracket(currentChar))
+                foundOpenBrackets += _openBrackets[currentChar];
+            
+            if(isClosedBracket(currentChar))
+                foundClosedBrackets += _closedBrackets[currentChar];
+        }
+        
+        return foundOpenBrackets == foundClosedBrackets;
+    }
+
+    private bool isOpenBracket(char character)
+    {
+        return character == '(' || character == '[' || character == '{';
+    }
+    
+    private bool isClosedBracket(char character)
+    {
+        return character == ')' || character == ']' || character == '}';
+    }
+}
---
+++ leetcode/cs/leetcode/s20/RunnerTest.cs
@@ -0,0 +1,57 @@
+﻿namespace leetcode.s20;
+
+using Xunit;
+
+public class RunnerTest
+{
+    private readonly Runner _runner;
+
+    public RunnerTest()
+    {
+        _runner = new Runner();
+    }
+    
+    [Fact]
+    public void Test1()
+    {
+        string input = "()";
+        var expected = true;
+
+        var actual = _runner.IsValid(input);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        string input = "()[]{}";
+        var expected = true;
+
+        var actual = _runner.IsValid(input);
+
+        Assert.Equal(expected, actual);
+    }
+    
+    [Fact]
+    public void Test3()
+    {
+        string input = "(]";
+        var expected = false;
+
+        var actual = _runner.IsValid(input);
+
+        Assert.Equal(expected, actual);
+    }
+    
+    [Fact]
+    public void Test4()
+    {
+        string input = "([])";
+        var expected = true;
+
+        var actual = _runner.IsValid(input);
+
+        Assert.Equal(expected, actual);
+    }
+}

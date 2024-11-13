8b12fdc0295fc1be510ce9747d97fab9659c2d49
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-11-13 21:17:51 +0200

    add unity getting started project

---
+++ unity/getting-started/GettingStarted/Assets/Printing.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Printing : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Hello world from Start()");
+    }
+
+    void Update()
+    {
+
+    }
+}

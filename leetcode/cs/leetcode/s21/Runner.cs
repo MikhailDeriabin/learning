4e1fdc86ed1cbaa61db75f56ace70982468658f2
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-11-15 13:58:21 +0200

    add base for s21

---
+++ leetcode/cs/leetcode/s21/Runner.cs
@@ -0,0 +1,78 @@
+﻿namespace leetcode.s21;
+
+using System;
+
+public class Runner
+{
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        return new ListNode();
+    }
+}
+
+public class ListNode : IEquatable<ListNode>
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+    
+    public override bool Equals(object obj) => this.Equals(obj as ListNode);
+
+    public bool Equals(ListNode node)
+    {
+        if (node is null)
+            return false;
+        
+        if (Object.ReferenceEquals(this, node))
+            return true;
+        
+        if (this.GetType() != node.GetType())
+            return false;
+        
+        return AreSameLists(this, node);
+    }
+
+    public static bool operator ==(ListNode left, ListNode right)
+    {
+        if (left is null)
+        {
+            if (right is null)
+                return true;
+
+            return false;
+        }
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(ListNode lhs, ListNode rhs) => !(lhs == rhs);
+
+    bool AreSameLists(ListNode list1, ListNode list2)
+    {
+        if ((list1 == null && list2 == null) || (list1 == null && list2 != null) || (list1 != null && list2 == null))
+            return false;
+        
+        
+        var list1Pointer = list1;
+        var list2Pointer = list2;
+        
+        while (list1Pointer != null || list2Pointer != null)
+        {
+            if (list1Pointer.val != list2Pointer.val)
+                return false;
+            
+            
+            list1Pointer = list1Pointer.next;
+            list2Pointer = list2Pointer.next;
+
+            if ((list1Pointer == null && list2Pointer != null) || (list1Pointer != null && list2Pointer == null))
+                return false;
+        }
+        
+        return true;
+    }
+}
---
+++ leetcode/cs/leetcode/s21/RunnerTest.cs
@@ -0,0 +1,71 @@
+﻿namespace leetcode.s21;
+
+using Xunit;
+
+public class RunnerTest
+{
+    private readonly Runner _runner;
+
+    public RunnerTest()
+    {
+        _runner = new Runner();
+    }
+    
+    [Fact]
+    public void Test1()
+    {
+        ListNode input1 = GenerateListNode([1,2,4]);
+        ListNode input2 = GenerateListNode([1,3,4]);
+        ListNode expected =  GenerateListNode([1,1,2,3,4,4]);
+
+        var actual = _runner.MergeTwoLists(input1, input2);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        ListNode input1 = GenerateListNode([]);
+        ListNode input2 = GenerateListNode([]);
+        ListNode expected =  GenerateListNode([]);
+
+        var actual = _runner.MergeTwoLists(input1, input2);
+
+        Assert.Equal(expected, actual);
+    }
+    
+    [Fact]
+    public void Test3()
+    {
+        ListNode input1 = GenerateListNode([]);
+        ListNode input2 = GenerateListNode([0]);
+        ListNode expected =  GenerateListNode([0]);
+
+        var actual = _runner.MergeTwoLists(input1, input2);
+
+        Assert.Equal(expected, actual);
+    }
+
+    ListNode GenerateListNode(int[] nodes)
+    {
+        if(nodes.Length == 0)
+            return null;
+        
+        if(nodes.Length == 1)
+            return new ListNode(nodes[0]);
+        
+        var head = new ListNode(nodes[0]);
+
+        var currentPointer = head;
+        
+        for (var i = 1; i < nodes.Length; i++)
+        {
+            var node = new ListNode(nodes[i]);
+            currentPointer.next = node;
+            
+            currentPointer = node;
+        }
+        return head;
+    }
+}

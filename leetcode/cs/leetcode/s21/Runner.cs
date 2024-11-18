912924e80a47447bc87b82107bcecdd9eae7e88f
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-11-18 13:32:36 +0200

    add leetcode s21

--- leetcode/cs/leetcode/s21/Runner.cs
+++ leetcode/cs/leetcode/s21/Runner.cs
@@ -6,7 +6,24 @@ public class Runner
 {
     public ListNode MergeTwoLists(ListNode list1, ListNode list2)
     {
-        return new ListNode();
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+        
+        if (list1 != null) current.next = list1;
+        else current.next = list2;
+        
+        return dummy.next;
     }
 }
 
@@ -20,20 +37,20 @@ public class ListNode : IEquatable<ListNode>
         this.val = val;
         this.next = next;
     }
-    
+
     public override bool Equals(object obj) => this.Equals(obj as ListNode);
 
     public bool Equals(ListNode node)
     {
         if (node is null)
             return false;
-        
+
         if (Object.ReferenceEquals(this, node))
             return true;
-        
+
         if (this.GetType() != node.GetType())
             return false;
-        
+
         return AreSameLists(this, node);
     }
 
@@ -46,6 +63,7 @@ public class ListNode : IEquatable<ListNode>
 
             return false;
         }
+
         return left.Equals(right);
     }
 
@@ -55,24 +73,37 @@ public class ListNode : IEquatable<ListNode>
     {
         if ((list1 == null && list2 == null) || (list1 == null && list2 != null) || (list1 != null && list2 == null))
             return false;
-        
-        
+
+
         var list1Pointer = list1;
         var list2Pointer = list2;
-        
+
         while (list1Pointer != null || list2Pointer != null)
         {
             if (list1Pointer.val != list2Pointer.val)
                 return false;
-            
-            
+
+
             list1Pointer = list1Pointer.next;
             list2Pointer = list2Pointer.next;
 
             if ((list1Pointer == null && list2Pointer != null) || (list1Pointer != null && list2Pointer == null))
                 return false;
         }
-        
+
         return true;
+    }
+
+    public override string ToString()
+    {
+        ListNode pointer = this;
+        var result = "";
+        while (pointer != null)
+        {
+            result += pointer.val + ", ";
+            pointer = pointer.next;
+        }
+
+        return result;
     }
 }
